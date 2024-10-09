e152b805d0a33b6532b0de2bbc9a6d0cee00eded
francis1009 <francism56789@gmail.com>
2024-10-10 01:22:44 +0800

    fix: add ngrok header

--- src/app/http-common.ts
+++ src/app/http-common.ts
@@ -4,6 +4,7 @@ const apiClient: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND,
   headers: {
     "Content-type": "application/json",
+    "ngrok-skip-browser-warning": "true",
   },
 });
 
